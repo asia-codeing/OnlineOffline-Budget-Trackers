44f41313617bce6eec9039c792b997e14cbb3ad5
Asia Alnahi <asia.alius@gmail.com>
2021-06-10 12:54:54 -0700

    Start working on the fetching function in the service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -34,23 +34,27 @@ self.addEventListener('activate', (event) => {
   );
  self.clients.claim();
 });
- 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+//Fetch
+self.addEventListener(fetch, (event) => {
+  if(event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME)
+      .then(cache => {
+       return fetch(event.request)
+       .then(response => {
+         if(response.status === 200) {
+           cache.put(event.request.url, response.clone());
+         }
+        return response 
+       })
+      .catch(err => {
+        return cache.match(event.request);
+      });
+    }).catch(err => console.log(err))
+  );
+  return;
   }
-});
+
+  event.respondWith
+})
