df7b9e20e8895346c27aaed114e8429444fec654
Asia Alnahi <asia.alius@gmail.com>
2021-06-09 18:18:48 -0700

    update the service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,9 @@
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/dist/app.bundle.js',
-  '/dist/favorites.bundle.js',
-  '/dist/topic.bundle.js',
-  'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
 const PRECACHE = 'precache-v1';
