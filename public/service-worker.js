d8d22c170e47e2d197fdc8b46a6fa70a957d3bda
Asia Alnahi <asia.alius@gmail.com>
2021-06-11 14:30:34 -0700

    working on the app to fix the ofline

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,9 @@ const FILES_TO_CACHE = [
   "/", 
   "/index.html", 
   "/index.js",  
-  "/styles.css", 
+  "/styles.css",
+  "db.js", 
+  "/manifest.webmanifest",
   "/icons/icon-192x192.png", 
   "/icons/icon-512x512.png"
 ];
